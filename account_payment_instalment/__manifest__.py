fe2db8cfec56ac8a09b7b0d3242405316e995945
imahmoud@white-code.co.uk <imahmoud@white-code.co.uk>
2020-09-06 14:36:08 +0200

    update to add module for payment instalments

---
+++ account_payment_instalment/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#
+{
+    'name': "Instalment in Payment Terms",
+    'version': '13.0.1.0.0',
+    'author': 'Raqami(ibrahim mohamed)',
+    'company': '',
+    'maintainer': '',
+    'website': '',
+    'category': 'Accounting',
+    'summary': 'Instalment in Payment Terms"',
+    'description': """
+    New type 'Instalment' in Odoo payment terms, Instalment in Payment Terms,Installment
+    """,
+    'depends': ['account'],
+    'data': [
+        'views/account_payment_term_view.xml',
+    ],
+    'images': [],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
